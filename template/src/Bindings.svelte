2969787ba205a451944d239235c151055a4db38a
503134279 <Swapnesh.Sangle1@ge.com>
2019-07-25 09:59:26 +0530

    Bindings Complete

--- template/src/Bindings.svelte
+++ template/src/Bindings.svelte
@@ -1,4 +1,7 @@
 <script>
+  // import MediaBindings from './MediaBindings.svelte';
+  // import Canvasbinding from './Canvasbinding.svelte';
+
   let name = "world";
   let a = 1;
   let b = 2;
@@ -14,7 +17,44 @@
       flavours[flavours.length - 1]
     }`;
   }
+
+  let textAreaValue = `This is a textarea statement`;
+
+  let questions = [
+    { id: 1, text: `Where did you go to school?` },
+    { id: 2, text: `What is your mother's name?` },
+    {
+      id: 3,
+      text: `What is another personal fact that an attacker could easily find with Google?`
+    }
+  ];
+
+  let selected = 1;
+  let cities = ["Mumbai", "Bangalore", "Delhi"];
+  let selectedCities = ["Mumbai"];
+  let contentHtml = "<p>Write some text!</p>";
+
+  let todos = [
+    { done: false, text: "finish Svelte tutorial" },
+    { done: false, text: "build an app" },
+    { done: false, text: "world domination" }
+  ];
+
+  $: todoRemaining = todos.filter(t => !t.done).length;
 </script>
+
+<style>
+  textarea {
+    width: 50%;
+    height: 200px;
+  }
+
+  [contenteditable] {
+    padding: 0.5em;
+    border: 1px solid #eee;
+    border-radius: 4px;
+  }
+</style>
 
 <hr />
 <h4>Bindings Tutorial</h4>
@@ -81,3 +121,50 @@
     You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'} of {join(flavours)}
   </p>
 {/if}
+
+<hr />
+<h4>Textarea binding</h4>
+<textarea bind:value={textAreaValue} />
+<p>{textAreaValue}</p>
+
+<hr />
+<h4>Select Binding</h4>
+<select bind:value={selected}>
+  {#each questions as question}
+    <option value={question.id}>{question.text}</option>
+  {/each}
+</select>
+<p>Selected id: {selected}</p>
+
+<hr />
+<h4>Multiple Select binding</h4>
+<select multiple bind:value={selectedCities}>
+  {#each cities as city}
+    <option value={city}>{city}</option>
+  {/each}
+</select>
+{#each selectedCities as selectedCity}
+  <p>{selectedCity}</p>
+{/each}
+
+<hr />
+<h4>Content Editable Bindings</h4>
+<div contenteditable="true" bind:innerHTML={contentHtml} />
+<pre>{contentHtml}</pre>
+
+<hr />
+<h4>Each Block binding(Direct mutation)</h4>
+{#each todos as todo}
+  <input type="checkbox" bind:checked={todo.done} />
+  <input type="text" bind:value={todo.text} />
+  <p></p>
+{/each}
+<p>todo's remainining: {todoRemaining}</p>
+
+<!-- <hr>
+<h4>Media Bindings</h4>
+<MediaBindings /> -->
+
+<!-- <hr>
+<h4>Canvas Bindings</h4>
+<Canvasbinding /> -->
